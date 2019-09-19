2b4a8f043faee90ed6d48b500cde4f5a5bcfab23
KSK <kurtknosala@gmail.com>
2019-09-19 15:38:03 -0600

    handlebar display working, image not working

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var orm = require('./config/orm');
+var exphbs = require('express-handlebars');
 
 var app = express();
 
@@ -8,7 +9,15 @@ var PORT = process.env.PORT || 8000;
 app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.jason());
+app.use(express.json());
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
 app.set('view engine', 'handlebars');
+
+app.get('/', function(req, res) {
+    res.render('index');
+})
+
+app.listen(PORT, function() {
+    console.log('Server listening on: http://localhost:' + PORT);
+})

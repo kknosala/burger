e44167a621dfd34ce89de53b151194d97c78f8b4
KSK <kurtknosala@gmail.com>
2019-09-19 12:34:45 -0600

    server initial setup in progress

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 var express = require('express');
+var orm = require('./config/orm');
+
+var app = express();
+
+var PORT = process.env.PORT || 8000;
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.jason());
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
+app.set('view engine', 'handlebars');

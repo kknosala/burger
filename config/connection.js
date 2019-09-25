aa39138997fc9100412d9d6dbdee09dc82a80d64
KSK <kurtknosala@gmail.com>
2019-09-24 18:28:31 -0600

    jawsDB more setup

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,11 @@
 var mysql = require ('mysql');
 
 var connection = mysql.createConnection({
-    host: 'localhost',
+    host: 'otmaa16c1i9nwrek.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     port: 3306,
-    user: 'root',
-    password: 'rootroot',
-    database: 'burgers_db',
-    use_env_variable: "JAWSDB_URL"
+    user: 'jsdg7r9vvu6yediv',
+    password: 'qo9pejdqhwo6hwh8',
+    database: '	p67zncgwksd6uh29'
 })
 
 connection.connect(function(err) {
